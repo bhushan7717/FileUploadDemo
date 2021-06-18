ec58cb30cdea9c8fcd61ee9e346133e67c5bdeeb
bhushan <bhushan.wadile@yahoo.com>
2021-06-18 17:11:56 +0530

    File Upload working

--- Controllers/FilesController.cs
+++ Controllers/FilesController.cs
@@ -1,6 +1,7 @@
 ﻿using FileUploadDemo.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -16,9 +17,21 @@ namespace FileUploadDemo.Controllers
             return View();
         }
         [HttpPost]
-        public ActionResult UploadFiles(FileViewModel fileViewModel)
+        public ActionResult UploadFiles(HttpPostedFileBase file)
         {
+                if(file != null)
+                { 
+                string fileName = Path.GetFileName(file.FileName);
+                string path = Path.Combine(Server.MapPath("~/UploadedFiles"), fileName);
+                file.SaveAs(path);
+                ViewBag.Message = "File uploaded successfully";
+                return View();
+                }
             return View();
         }
-    }
+		protected override void OnException(ExceptionContext filterContext)
+		{
+			
+		}
+	}
 }
