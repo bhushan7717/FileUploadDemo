3db0eb08ac2d049d2f9c165623acebaf285036de
bhushan <bhushan.wadile@yahoo.com>
2021-06-18 22:01:50 +0530

    login controller UI added

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,35 @@
+﻿using FileUploadDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FileUploadDemo.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                return View();
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/LoginViewModel.cs
@@ -0,0 +1,18 @@
+﻿
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace FileUploadDemo.Models
+{
+	public class LoginViewModel
+	{
+		[DisplayName("User Name")]
+		[Required]
+		public string UserName { get; set; }
+		[DisplayName("Password")]
+		[Required]
+		[DataType(DataType.Password)]
+		public string Password { get; set; }
+
+	}
+}
