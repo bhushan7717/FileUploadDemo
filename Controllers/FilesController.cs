c1afe5099d45fde59d042cea02bf14116e35dc68
bhushan <bhushan.wadile@yahoo.com>
2021-06-18 12:45:01 +0530

    Add project files.

---
+++ Controllers/FilesController.cs
@@ -0,0 +1,24 @@
+﻿using FileUploadDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FileUploadDemo.Controllers
+{
+    public class FilesController : Controller
+    {
+        // GET: Files
+        [HttpGet]
+        public ActionResult UploadFiles()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult UploadFiles(FileViewModel fileViewModel)
+        {
+            return View();
+        }
+    }
+}
